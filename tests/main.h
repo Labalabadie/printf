e004b869d5507cf94471939e818b0ac4749b130d
ayamorcito <izquierdoz.g@gmail.com>
2022-03-12 07:55:38 -0800

    main1

--- tests/main.h
+++ tests/main.h
@@ -1,2 +1,17 @@
 int _putchar(char c);
 int _printf(const char * format, ...);
+int print_c(char c);
+
+/**
+ *
+ *
+ *
+ **/
+
+typedef struct pfunc
+{
+	char c;
+	void (*f)(va_list);
+} pfunc;
+
+
