2348fe9819f31bf40791ab90ac55770e547821c5
ayamorcito <izquierdoz.g@gmail.com>
2022-03-12 14:51:36 -0800

    bettymain

--- tests/main.h
+++ tests/main.h
@@ -1,15 +1,23 @@
+#ifndef LABA_AMORCITO
+#define LABA_AMORCITO
+
 #include <stdarg.h>
 #include <stdio.h>
+
+
 int _putchar(char c);
-int _printf(const char * format, ...);
+int _printf(const char *format, ...);
 int print_c(va_list  arg);
 int _strlen(char *s);
-int print_s();
+int print_s(va_list arg);
+int print_i(va_list arg);
+
+
 
 /**
- *
- *
- *
+ * struct pfunc - model for structs
+ * @x: character to compare
+ * @f: function to do in case of match
  **/
 
 typedef struct pfunc
@@ -18,4 +26,4 @@ typedef struct pfunc
 	int (*f)(va_list arg);
 } pfunc;
 
-
+#endif
