d4c40b8113891fb63ab9dd3aef0190d9e344082e
Agustin Labadie <4433@holbertonschool.com>
2022-03-15 05:31:21 -0700

    fixed negatives on main

--- print_i.c
+++ print_i.c
@@ -12,14 +12,17 @@ int print_i(va_list arg)
 	int a = 0;
 	int counter = 0;
 	
-	if (!i)
-		return(0);
 	if (i == 0 || i == '\0')
 		return	(_putchar('0'));
 	if (i < 0)
 	{
-		return	(print_ui(i));
+/*		return	(print_ui(i));*/
+		i = -i;
+		_putchar('-');
+		counter++;
+		j = i;
 	}
+
 	while (i > 0)
 	{
 		i /= 10;
