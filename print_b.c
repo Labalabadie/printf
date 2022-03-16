c4900699c26ec7d7574d91710b200873ae76695e
Agustin Labadie <4433@holbertonschool.com>
2022-03-16 10:46:46 -0700

    handles negative

--- print_b.c
+++ print_b.c
@@ -10,15 +10,21 @@
 int print_b(va_list arg)
 {
 	int n = va_arg(arg, int);
-	int i;
-	int j;
+	unsigned int i;
+	unsigned int j;
 	char *a;
-	int m = n;
+	unsigned int m = n;
 	int counter = 0;
 
 	if (n == 0)
 		return(_putchar('0'));
-
+	if (n < 0)
+	{
+		n = - n;
+		_putchar('-');
+		counter++;
+	}
+	m = n;
 	for (i = 0; n > 0; i++)
 	{
 		n /=  2;
