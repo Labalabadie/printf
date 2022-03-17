d576a2d70a4187a23f812b8b70def053dfa254fd
Agustin Labadie <4433@holbertonschool.com>
2022-03-16 17:19:16 -0700

    Function %r

---
+++ print_r.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_r - Prints strings in reverse.
+ * @arg: arguments
+ * Return: string lenght
+ **/
+
+int print_r(va_list arg)
+{
+	char *a = va_arg(arg, char*);
+	int i = 0;
+	int counter = 0;
+	char *b;
+
+	if (!a)
+	{
+		return (_printf("(null)"));
+	}
+	b = a;
+	while (*a != '\0')
+	{
+		a++;
+		i++;
+	}
+	while (i >= 0)
+	{
+		_putchar(b[i]);
+		i--;
+		counter++;
+	}
+	return (counter);
+}
